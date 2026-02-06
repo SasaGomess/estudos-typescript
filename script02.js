e6ec32ba945f23585d729b1e4ea7804568f9af23
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-06 15:13:33 -0300

    feat: conteudo sobre tipos de inferencia e configurações básicas do typescript

---
+++ script02.js
@@ -0,0 +1,7 @@
+"use strict";
+let produto = "Livro";
+let preco = 34;
+const carro = {
+    marca: "Audi",
+    portas: 4
+};
---
+++ script02.ts
@@ -0,0 +1,13 @@
+let produto= "Livro";
+
+let preco = 34;
+
+const carro: {
+    marca: string;
+    portas: number;
+} = {
+    marca: "Audi",
+    portas: 4
+}
+
+
