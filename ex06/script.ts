b28f740dadf617356d49d582d8fb413133e27f76
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-06 22:36:26 -0300

    feat: conteudo sobre union types e optional chaining

---
+++ ex06/script.ts
@@ -0,0 +1,17 @@
+export let total: string | number = 200;
+
+total = '500';
+
+function isNumber(total: string | number ) {
+    if(typeof total === 'number'){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+console.log(isNumber("200"));
+
+const button = document.querySelector('button');
+
+button?.click();
