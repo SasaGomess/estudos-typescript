36878fcc1553b02ab4abce0d66637e9bd47537c9
Sabrina Gomes <sabrina.gomess.dev@gmail.com>
2026-02-08 14:37:31 -0300

    feat: trabalha com intanceof com getElementById

---
+++ ex16/script.ts
@@ -0,0 +1,15 @@
+function mudaProtocolo(){
+    const link = document.getElementById("origamid");
+
+    console.dir(link);
+    if (link instanceof HTMLAnchorElement) {
+       link.href = link.href.replace("http", "https");
+    }
+}
+mudaProtocolo();
+
+
+
+
+
+
