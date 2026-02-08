8abce63ba04fde7dac423d726471ed076d0b52f7
Sabrina Gomes <sabrina.gomess.dev@gmail.com>
2026-02-08 14:53:00 -0300

    feat: conteudo sobre DOM utilizando queryselector com instanceof nos seus possiveis retornos e hierarquias de classe

---
+++ ex17/script.ts
@@ -0,0 +1,11 @@
+const video = document.querySelector("video");
+const videoPrincipal = document.querySelector("#videoprincipal");
+
+if(videoPrincipal instanceof HTMLVideoElement) {
+    console.log(videoPrincipal.volume);
+}
+
+
+
+
+
