477928fd3a66377dece341ac33eda5c0460ad1af
Sabrina Gomes <sabrina.gomess.dev@gmail.com>
2026-02-08 15:26:44 -0300

    feat: add conteudo sobre NodeList e suas diferencas com a interface e com a estrutura de dados array

---
+++ ex18/script.ts
@@ -0,0 +1,20 @@
+const links = document.querySelectorAll(".link");
+
+console.log(NodeList);
+
+links.forEach(link => {
+    if(link instanceof HTMLAnchorElement){
+        console.log(link.href)
+    } 
+    
+})
+
+const dados = [1, 2, 3];
+
+const arrayLinks = Array.from(links);
+
+const anchorLinks = arrayLinks.filter(a => a instanceof HTMLAnchorElement);
+
+console.log(anchorLinks);
+
+
